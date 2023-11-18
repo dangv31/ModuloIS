b2cbae8339e91e2545de02a6dd334191e4d48837
dangv31 <124928158+dangv31@users.noreply.github.com>
2023-11-18 14:38:23 -0500

    atributos cuenta

--- src/gestor_aplicacion/Cuenta.py
+++ src/gestor_aplicacion/Cuenta.py
@@ -1,2 +1,11 @@
 class Cuenta():
-    def __init__(self):
+    def __init__(self, nombres, apellidos, doc, nacimiento, contraseña, correo, rol):
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.doc_identidad = doc
+        self.nacimiento = nacimiento
+        self.contraseña = contraseña
+        self.correo = correo
+        self.rol = rol
+        self.estado = True
+        self.observaciones = []
