89fe345ea74ec271566bad137e17e17d02c8abae
DiegoGG512 <124928460+DiegoGG512@users.noreply.github.com>
2023-11-20 15:07:15 -0500

    casos de error login y menus

--- src/ui_main/Inicio_sesion.py
+++ src/ui_main/Inicio_sesion.py
@@ -1,20 +1,37 @@
 from src.gestor_aplicacion.Cuenta import Cuenta
 from src.ui_main.Menu_inicial import Menu_inicial
 
-
 class Inicio_sesion:
     @classmethod
     def inicio_sesion(cls):
-        correo_ingresado = input("Ingrese su correo: ")
-        contraseña_ingresada = input("Ingrese su contraseña: ")
+        while True:
+            print()
+            correo_ingresado = input("Ingrese su correo: ")
+            contraseña_ingresada = input("Ingrese su contraseña: ")
 
-        for cuenta in Cuenta.lista_cuentas:
-            if cuenta.correo == correo_ingresado and cuenta.contraseña == contraseña_ingresada and "Administrativo" in cuenta.rol:
-                print(f"¡Bienvenido {cuenta.nombres}!")
-                Menu_inicial.menu_inicial_Administrativo()
+            cuenta_encontrada = None
 
-            if cuenta.correo == correo_ingresado and cuenta.contraseña == contraseña_ingresada and "Administrativo" not in cuenta.rol:
-                print(f"¡Bienvenido {cuenta.nombres}!")
-                Menu_inicial.menu_inicial_Clinico()
+            for cuenta in Cuenta.lista_cuentas:
+                if cuenta.correo == correo_ingresado and cuenta.contraseña == contraseña_ingresada:
+                    cuenta_encontrada = cuenta
+                    break
+
+            if cuenta_encontrada:
+                if "Administrativo" in cuenta_encontrada.rol:
+                    print()
+                    print(f"¡Bienvenido {cuenta_encontrada.nombres}!")
+                    Menu_inicial.menu_inicial_Administrativo()
+                else:
+                    print()
+                    print(f"¡Bienvenido {cuenta_encontrada.nombres}!")
+                    Menu_inicial.menu_inicial_Clinico()
+                break
+            else:
+                print()
+                print("Error: Correo o contraseña incorrectos.")
+                opcion = input("¿Desea intentarlo de nuevo? (s/n): ").lower()
+                if opcion != 's':
+                    print("Saliendo del programa. ¡Hasta luego!")
+                    break
 
 Inicio_sesion.inicio_sesion()
--- src/ui_main/Menu_inicial.py
+++ src/ui_main/Menu_inicial.py
@@ -23,26 +23,44 @@ Sede.lista_sedes.append(sede3)
 class Menu_inicial:
     @classmethod
     def menu_inicial_Administrativo(cls):
-        print("1. Gestionar Cuentas")
-        print("2. Gestionar Maestros")
-        print("3. Gestionar sedes")
-        opcion = input("Seleccione una opcion: ")
-        print()
-        if opcion == "1":
-            Menu_Gestion_Cuentas.menu_gestion_cuentas(cuenta)
-        if opcion == "2":
-            Menu_Gestion_Maestros.menu_gestion_maestros(cuenta)
-        if opcion == "3":
-            Menu_Gestion_Sede.menu_gestion_sede(cuenta)
+        while True:
+            print()
+            print("1. Gestionar Cuentas")
+            print("2. Gestionar Maestros")
+            print("3. Gestionar sedes")
+            print("4. Salir")
+            opcion = input("Seleccione una opcion: ")
+            print()
+
+            if opcion == "1":
+                Menu_Gestion_Cuentas.menu_gestion_cuentas(cuenta)
+            elif opcion == "2":
+                Menu_Gestion_Maestros.menu_gestion_maestros(cuenta)
+            elif opcion == "3":
+                Menu_Gestion_Sede.menu_gestion_sede(cuenta)
+            elif opcion == "4":
+                print("Saliendo...")
+                break
+            else:
+                print("Opción no válida. Por favor, seleccione una opción válida.")
 
     @classmethod
     def menu_inicial_Clinico(cls):
-        print("1. Ver Maestro")
-        print("2. Ver sede")
-        opcion = input("Seleccione una opcion: ")
-        print()
-        if opcion == "1":
-            Ver_Maestro.ver_maestro(cuenta)
-        if opcion == "2":
-            Ver_Sede.ver_sede(cuenta)
+        while True:
+            print()
+            print("1. Ver Maestro")
+            print("2. Ver sede")
+            print("3. Salir")
+            opcion = input("Seleccione una opcion: ")
+            print()
+
+            if opcion == "1":
+                Ver_Maestro.ver_maestro(cuenta)
+            elif opcion == "2":
+                Ver_Sede.ver_sede(cuenta)
+            elif opcion == "3":
+                print("Saliendo...")
+                break
+            else:
+                print("Opción no válida. Por favor, seleccione una opción válida.")
 
